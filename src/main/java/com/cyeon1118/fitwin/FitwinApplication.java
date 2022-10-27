2ef0e9311c0ce56d00543ff715c9b75ec511b096
cyeon1118 <78338944+cyeon1118@users.noreply.github.com>
2022-10-27 20:38:50 +0900

    Initial commit

---
+++ src/main/java/com/cyeon1118/fitwin/FitwinApplication.java
@@ -0,0 +1,13 @@
+package com.cyeon1118.fitwin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitwinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitwinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cyeon1118/fitwin/FitwinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cyeon1118.fitwin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitwinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
